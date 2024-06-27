17524d4b5c52e0244dcd85865f0332ba17da814e
jason <jason_tan75@hotmail.com>
2024-06-27 15:22:23 -0400

    more consistent styling

--- controllers/foods.js
+++ controllers/foods.js
@@ -5,15 +5,11 @@ const User = require('../models/user.js');
 
 router.get('/', async (req, res) => {
     try {
-        // Look up the user from req.session
         const currentUser = await User.findById(req.session.user._id);
-        // Render index.ejs, passing in all of the current user's 
-        // foods as data in the context object. 
         res.render('foods/index.ejs', {
             foods: currentUser.pantry,
         });
     } catch (error) {
-        // If any errors, log them and redirect back home
         console.log(error)
         res.redirect('/')
     }
@@ -21,17 +17,11 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        // Look up the user from req.session
         const currentUser = await User.findById(req.session.user._id);
-        // Push req.body (the new form data object) to the
-        // foods array of the current user
         currentUser.pantry.push(req.body);
-        // Save changes to the user
         await currentUser.save();
-        // Redirect back to the foods index view
         res.redirect(`/users/${currentUser._id}/foods`);
     } catch (error) {
-        // If any errors, log them and redirect back home
         console.log(error);
         res.redirect('/')
     }
@@ -43,17 +33,11 @@ router.get('/new', (req, res) => {
 
 router.post('/', async (req, res) => {
     try {
-        // Look up the user from req.session
         const currentUser = await User.findById(req.session.user._id);
-        // Push req.body (the new form data object) to the
-        // foods array of the current user
         currentUser.pantry.push(req.body);
-        // Save changes to the user
         await currentUser.save();
-        // Redirect back to the foods index view
         res.redirect(`/users/${currentUser._id}/foods`);
     } catch (error) {
-        // If any errors, log them and redirect back home
         console.log(error);
         res.redirect('/')
     }
@@ -65,16 +49,12 @@ router.get('/new', async (req, res) => {
 
 router.get('/:foodId', async (req, res) => {
     try {
-        // Look up the user from req.session
         const currentUser = await User.findById(req.session.user._id);
-        // Find the food by the foodId supplied from req.params
         const food = currentUser.pantry.id(req.params.foodId);
-        // Render the show view, passing the food data in the context object
         res.render('foods/show.ejs', {
             food: food,
         });
     } catch (error) {
-        // If any errors, log them and redirect back home
         console.log(error);
         res.redirect('/')
     }
@@ -82,17 +62,11 @@ router.get('/:foodId', async (req, res) => {
 
 router.delete('/:foodId', async (req, res) => {
     try {
-        // Look up the user from req.session
         const currentUser = await User.findById(req.session.user._id);
-        // Use the Mongoose .deleteOne() method to delete 
-        // an food using the id supplied from req.params
         currentUser.pantry.id(req.params.foodId).deleteOne();
-        // Save changes to the user
         await currentUser.save();
-        // Redirect back to the foods index view
         res.redirect(`/users/${currentUser._id}/foods`);
     } catch (error) {
-        // If any errors, log them and redirect back home
         console.log(error);
         res.redirect('/')
     }
@@ -114,15 +88,11 @@ router.get('/:foodId/edit', async (req, res) => {
 
 router.put('/:foodId', async (req, res) => {
     try {
-        // Find the user from req.session
         const currentUser = await User.findById(req.session.user._id);
-        // Find the current food from the id supplied by req.params
         const food = currentUser.pantry.id(req.params.foodId);
-        // Use the Mongoose .set() method, updating the current food to reflect the new form data on `req.body`
         food.set(req.body);
         // Save the current user
         await currentUser.save();
-        // Redirect back to the show view of the current food
         res.redirect(
             `/users/${currentUser._id}/foods/${req.params.foodId}`
         );
